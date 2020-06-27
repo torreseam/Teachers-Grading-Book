3dcf336bde86a62ea1f16fec4709b4a62fedddcf
Russell Morgan <rhmorgan13@comcast.net>
2020-06-27 08:57:09 -0500

    adding updated models and course-routes

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,16 @@
-use teacher_grade_book_db;
+use grade_book_db;
 
-insert into teacher (
+insert into teacher 
     (name, email, password)
-    values('Mary Shelley', 'mshelley@franklinhs.com', 'frankenstein')
-);
+    values('Mary Shelley', 'mshelley@franklinhs.com', 'frank');
+
+insert into course (name, teacher_id, department)
+    values('Biology', 1, 'Science');
+insert into course (name, teacher_id, department)
+    values('Calculus', 1, 'Mathematics');
 
 insert into student (name) values('Mary Andrews');
-insert into student (name) values('Tom Swith');
+insert into student (name) values('Tom Swift');
 insert into student (name) values('Barbara Johnson');
 insert into student (name) values('Rick Shaw');
 insert into student (name) values('Mike Thompson');
@@ -16,16 +20,14 @@ insert into student (name) values('Joan Simpson');
 insert into student (name) values('Leslie Patterson');
 insert into student (name) values('Chris Steward');
 
-insert into roster (id, student_id) values(1, 1);
-insert into roster (id, student_id) values(1, 2);
-insert into roster (id, student_id) values(1, 3);
-insert into roster (id, student_id) values(1, 4);
-insert into roster (id, student_id) values(1, 5);
-insert into roster (id, student_id) values(1, 6);
-insert into roster (id, student_id) values(1, 7);
-insert into roster (id, student_id) values(1, 8);
-insert into roster (id, student_id) values(1, 9);
-insert into roster (id, student_id) values(1, 10);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(1, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(1, 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(1, 5, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(1, 7, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(1, 9, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-insert into course (name, teacher_id, roster_id, department)
-    values('Biology', 1, 1, 'Science');
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(2, 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(2, 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(2, 6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(2, 8, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+insert into courseStudents (course_id, student_id, createdAt, updatedAt) values(2, 10, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
--- models/index.js
+++ models/index.js
@@ -1,8 +1,56 @@
+const sequelize = require('../config/connection');
+const { DataTypes } = require('sequelize');
 const Teacher = require('./Teacher');
 const Course  = require('./Course');
 const Student = require('./Student');
-const Roster  = require('./Roster');
 const Grade   = require('./Grade');
 
+Teacher.hasMany(Course, {
+  foreignKey: 'teacher_id'
+});
 
-module.exports = { Teacher, Course, Student, Roster, Grade };
+Course.belongsTo(Teacher, {
+  foreignKey: 'teacher_id',
+});
+
+CourseStudent = sequelize.define('courseStudent', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+    unique: true
+  },
+ course_id: {
+    type: DataTypes.INTEGER,
+    references: 'Course',
+    referencesKey: 'id',
+    allowNull: false
+  },
+  student_id: {
+    type: DataTypes.INTEGER,
+    references: 'Student',
+    referencesKey: 'id',
+    allowNull: false
+  }
+});
+
+//Course.belongsToMany(Student, { through: courseStudent });
+Course.belongsToMany(
+  Student, 
+  {
+      through: 'courseStudent',
+      foreignKey: 'course_id'
+  }
+)
+
+//Student.belongsToMany(Course, { through: courseStudent });
+Student.belongsToMany(
+  Course, 
+  {
+      through: 'courseStudent',
+      foreignKey: 'student_id'
+  }
+)
+
+module.exports = { Teacher, Course, Student, CourseStudent, Grade };
