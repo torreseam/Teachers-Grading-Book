7b6cbd83467d08213013d3e6abf2ec6f091e9430
mattkohl82 <mattkohl82@gmail.com>
2020-06-30 15:59:01 -0500

    made some routes for user and courses

--- models/index.js
+++ models/index.js
@@ -1,56 +1,13 @@
-const sequelize = require('../config/connection');
-const { DataTypes } = require('sequelize');
-const Teacher = require('./Teacher');
-const Course  = require('./Course');
-const Student = require('./Student');
-const Grade   = require('./Grade');
+const User = require("./User");
+const Course = require("./Course");
 
-Teacher.hasMany(Course, {
-  foreignKey: 'teacher_id'
+// create associations
+User.hasMany(Course, {
+  foreignKey: 'user_id'
 });
 
-Course.belongsTo(Teacher, {
-  foreignKey: 'teacher_id',
+Course.belongsTo(User, {
+  foreignKey: 'user_id',
 });
 
-CourseStudent = sequelize.define('courseStudent', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    allowNull: false,
-    autoIncrement: true,
-    unique: true
-  },
- course_id: {
-    type: DataTypes.INTEGER,
-    references: 'Course',
-    referencesKey: 'id',
-    allowNull: false
-  },
-  student_id: {
-    type: DataTypes.INTEGER,
-    references: 'Student',
-    referencesKey: 'id',
-    allowNull: false
-  }
-});
-
-//Course.belongsToMany(Student, { through: courseStudent });
-Course.belongsToMany(
-  Student, 
-  {
-      through: 'courseStudent',
-      foreignKey: 'course_id'
-  }
-)
-
-//Student.belongsToMany(Course, { through: courseStudent });
-Student.belongsToMany(
-  Course, 
-  {
-      through: 'courseStudent',
-      foreignKey: 'student_id'
-  }
-)
-
-module.exports = { Teacher, Course, Student, CourseStudent, Grade };
+module.exports = { User, Course };
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+const userRoutes = require('./user-routes');
 const courseRoutes = require('./course-routes');
 
+router.use('/users', userRoutes);
 router.use('/courses', courseRoutes);
 
 module.exports = router;
