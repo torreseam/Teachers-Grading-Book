669d07134ff34d399be21215e759e6203ab82fad
mattkohl82 <mattkohl82@gmail.com>
2020-06-30 23:22:52 -0500

    routes and handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const coursesRoutes = require('./courses-routes.js');
+const homeRoutes = require('./home-routes.js');
+
 
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
+router.use('/', coursesRoutes);
+router.use('/', homeRoutes);
+
 
 router.use((req, res) => {
   res.status(404).end();
